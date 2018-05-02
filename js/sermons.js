87be3eaed87a7a51f15f9a1ba129b9df9c4f90e2
Yves <yvesfrancisco95@gmail.com>
2018-05-02 00:02:07 -0800

    moved devotional from script.js to sermons.js

--- js/sermons.js
+++ js/sermons.js
@@ -1,27 +1,78 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var container = document.getElementsByClassName('back-to-top-container')[0];
-    var backToTopButton = document.getElementById('back-to-top-button');
+document.addEventListener("DOMContentLoaded", function() {
+    var backTop = function() {
+        var container = document.getElementsByClassName("back-to-top-container")[0];
+        var backToTopButton = document.getElementById("back-to-top-button");
 
-    console.log(container);
-
-    backToTopButton.addEventListener('click', function () {
-        document.querySelector(this.dataset.target).scrollIntoView({
-            behavior: 'smooth'
+        backToTopButton.addEventListener("click", function() {
+            document.querySelector(this.dataset.target).scrollIntoView({
+                behavior: "smooth"
+            });
         });
-    });
 
-    window.addEventListener('scroll', buttonPosition);
+        window.addEventListener("scroll", buttonPosition);
 
-    function buttonPosition() {
-        if (window.scrollY >= container.offsetTop && window.scrollY + window.innerHeight < container.offsetTop + container.offsetHeight) {
-            backToTopButton.style.transform = "scale(1)";
-            backToTopButton.style.opacity = 1;
-            backToTopButton.style.position = "fixed";
-        } else if (window.scrollY + window.innerHeight >= container.offsetTop + container.offsetHeight) {
-            backToTopButton.style.position = "absolute";
-        } else {
-            backToTopButton.style.opacity = 0;
-            backToTopButton.style.transform = "scale(0)";
+        function buttonPosition() {
+            if (window.scrollY >= container.offsetTop && window.scrollY + window.innerHeight < container.offsetTop + container.offsetHeight) {
+                backToTopButton.style.transform = "scale(1)";
+                backToTopButton.style.opacity = 1;
+                backToTopButton.style.position = "fixed";
+            } else if (window.scrollY + window.innerHeight >= container.offsetTop + container.offsetHeight) {
+                backToTopButton.style.position = "absolute";
+            } else {
+                backToTopButton.style.opacity = 0;
+                backToTopButton.style.transform = "scale(0)";
+            }
         }
+    };
+
+    var dev = (function() {
+        var devotional = document.getElementById("morning-evening-devotional");
+
+        if (!devotional) {
+            return;
+        }
+
+        fetchJSONfile("morning-and-evening-master/m_e.json", function(data) {
+            var dateToday = new Date();
+            var day = dateToday.getDate();
+            var month = dateToday.getMonth() + 1;
+            var monthDateString = month + "-" + day;
+            var time;
+            if (dateToday.getHours < 12) {
+                time = "pm";
+            } else {
+                time = "am";
+            }
+            for (var i = 0; i < data.length; i++) {
+                var element = data[i];
+                if (
+                    element !== null &&
+                    element.month === month &&
+                    element.day === day &&
+                    element.time === time
+                ) {
+                    console.log(element);
+                    var bodyText = element.body.replace(/\n/g, "<br>");
+                    document.getElementById("dev-body").innerHTML = bodyText;
+                    document.getElementById("dev-keyverse").innerHTML =
+                        element.keyverse;
+                }
+            }
+        });
+    })();
+    
+
+    function fetchJSONfile(path, callback) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == "200") {
+                var data = JSON.parse(this.responseText);
+                if (callback) {
+                    callback(data);
+                }
+            }
+        };
+        xhttp.open("GET", path, true);
+        xhttp.send();
     }
 });
